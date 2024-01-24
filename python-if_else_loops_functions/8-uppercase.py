ad207cbef215e024f5c4c7e731bb1c48a899cdb8
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-24 22:29:19 +1100

    fix(checker): case '' to print \n

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,12 +3,15 @@
 def uppercase(str):
     counter = 0
     current = 0
-    separator = ""
+
     while (counter < len(str)):
         current = ord(str[counter])
-        separator = "" if counter != len(str) - 1 else "\n"
         if 97 <= current <= 122:
-            print("{}".format(chr(current - 32)), end=separator)
+            current = ord(str[counter]) - 32
         else:
-            print("{}".format(str[counter]), end=separator)
+            current = ord(str[counter])
+
+        print("{}".format(chr(current)), end="")
         counter = counter + 1
+
+    print("{}".format(""))
