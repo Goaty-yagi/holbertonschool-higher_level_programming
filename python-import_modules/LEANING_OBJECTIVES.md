f3d9afa9f4f34464f17bcf0f31a3d30ae1efa405
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-25 16:43:24 +1100

    feat: add 0-add

--- python-import_modules/LEANING_OBJECTIVES.md
+++ python-import_modules/LEANING_OBJECTIVES.md
@@ -10,12 +10,89 @@
 
 
 ## 1, Why Python programming is awesome
+Python programming is awesome for a variety of reasons, making it a popular and widely adopted language across different domains.
+- Readability and Simplicity:
+Python emphasizes readability and simplicity, making it easy to learn and understand. Its syntax allows developers to express concepts in fewer lines of code compared to other languages, reducing the likelihood of errors and enhancing code clarity.
+
+- High-Level Language:
+Python is a high-level language, abstracting away many low-level details. This makes it easier for developers to focus on solving problems rather than dealing with intricate system-specific nuances.
+
 ## 2, How to import functions from another file
+You can import functions from another file using the import statement. 
 ## 3, How to use imported functions
+Once you have imported functions from another file/module, you can use them in your current script or module as if they were defined locally.
+```python
+from test import add
+
+result = add(1, 2)
+print("Result: " result) # Result is 3
+```
 ## 4, How to create a module
+Creating a module in Python is essentially creating a Python script with reusable code and saving it with a .py extension. 
+
 ## 5, How to use the built-in function dir()
+### Listing Names in the Current Scope:
+When called without any arguments, dir() returns a list of names in the current local scope.
+
+```python
+# Example 1
+names_in_scope = dir()
+print(names_in_scope)
+
+```
+
+### Listing Names in a Module or Object:
+When called with an argument, dir() returns a list of names defined in that module or object.
+```python
+# Example 2
+import math
+
+math_names = dir(math)
+print(math_names)
+
+```
+
+### Interactive Exploration:
+dir() is often used interactively in the Python interpreter to explore the attributes of an object.
+```python
+# Example 3
+my_list = [1, 2, 3]
+
+# List the attributes and methods of the list object
+list_attributes = dir(my_list)
+print(list_attributes)
+
+```
+
 ## 6, How to prevent code in your script from being executed when imported
+You can use the if __name__ == "__main__": construct to prevent certain code from being executed when the script is imported as a module.
 ## 7, How to use command line arguments with your Python programs
+You can use the sys.argv list from the sys module.
+```python
+# my_script.py
+import sys
+
+# Access command line arguments using sys.argv
+# sys.argv[0] is the script name itself
+# sys.argv[1:], starting from index 1, are the command line arguments
+arguments = sys.argv[1:]
+
+# Print the command line arguments
+print("Command line arguments:", arguments)
+```
+
+Run the command.
+```bash
+python my_script.py arg1 arg2 arg3
+
+```
+Expected output.
+```bash
+Command line arguments: ['arg1', 'arg2', 'arg3']
+
+```
+
+
 
 
 
--- python-import_modules/tests/test0.py
+++ python-import_modules/tests/test0.py
@@ -1,26 +1,23 @@
 import unittest
 import os
 
-from utils import use_method, get_original_line, run_script, run_pycodestyle, count_constructs, count_variable_declarations
+from utils import replace_line, count_constructs, get_original_line, run_script, run_pycodestyle, use_method
+
+filename = "0-add.py"
 
 
-filename = "2-print_alphabet.py"
-
-
-class TestTask2(unittest.TestCase):
+class TestTask0(unittest.TestCase):
     def test_file_exist(self):
         """ Testing the target file exists. """
 
         self.assertTrue(os.path.exists(filename),
                         f"File '{filename}' does not exist")
 
-
     def test_is_executable(self):
         """ Testing the target file is executable. """
 
         is_executable = os.access(filename, os.X_OK)
         self.assertTrue(is_executable, f"{filename} should be executable")
-
 
     def test_readme_exist(self):
         """ Testing README.md exists in the current dir, and has contents. """
@@ -34,15 +31,13 @@ class TestTask2(unittest.TestCase):
         self.assertTrue(len(script_content),
                         f"File '{readme}' does not have contents")
 
-
     def test_pycode_style(self):
-        """ Testing a specific file meets pep8 requirements. """
+        """ Testing  a specific file meet pep8 requirements """
 
         result = run_pycodestyle(filename)
         # Check if pycodestyle result is successful (exit code 0)
         self.assertEqual(result.returncode, 0,
                          f"pycodestyle check failed:\n{result.stdout}")
-
 
     def test_first_line(self):
         """ Testing the first line of a specific file is expected. """
@@ -51,57 +46,21 @@ class TestTask2(unittest.TestCase):
         expected_line = "#!/usr/bin/python3\n"
         self.assertEqual(first_line, expected_line)
 
-
     def test_script_output(self):
         """ Testing a specific file output is expected. """
 
-        import string
-
         script_output = run_script(filename)
-        expected_output = string.ascii_lowercase
-
+        expected_output = "1 + 2 = 3"
         self.assertEqual(script_output, expected_output)
 
+    def test_count_star(self):
+        """ Testing if import * is used in the file. """
 
-    def test_count_print(self):
-        """ Testing  a number of print function in use in the file. """
-
-        target_construct = "print"
-        count_obj = count_constructs(filename, [target_construct])
-        expected_count = 1
-        self.assertEqual(count_obj[target_construct], expected_count,
-                         "There should be exactly one print statement.")
-
-
-    def test_count_loop(self):
-        """ Testing  a number of for or while loop in use in the file. """
-
-        target_constructs = ["for", "while"]
-        count_obj = count_constructs(filename, target_constructs)
-        expected_count = 1
-        total_count = count_obj["for"] + count_obj["while"]
-        self.assertEqual(total_count, expected_count,
-                         "There should be exactly one loop statement.")
-
-
-    def test_count_import(self):
-        """ Testing if import is used in the file. """
-
-        target_construct = "import"
+        target_construct = "import *"
         count_obj = count_constructs(filename, [target_construct])
         expected_count = 0
         self.assertEqual(count_obj[target_construct], expected_count,
-                         "There should be exactly no import statement.")
-
-
-    def test_variable_declarations(self):
-        """ Testing if variable is declared in the file. """
-
-        variable_count = count_variable_declarations(filename)
-        expected_count = 0
-        self.assertEqual(variable_count, expected_count,
-                         f"Expected variable declaration count: {expected_count}")
-
+                         f"There shouldn't be <{target_construct}> statement in use.")
 
     def test_format_in_use(self):
         """ Testing the format method is used in the file. """
