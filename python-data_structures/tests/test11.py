3b86ce1350c84cb53fd6935e7c1646ae73a93af2
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-26 23:22:24 +1100

    feat: add 11-delete_at

---
+++ python-data_structures/tests/test11.py
@@ -0,0 +1,57 @@
+import unittest
+import importlib
+import sys
+import os
+
+from parent_test import ParentTest
+
+filename = "11-delete_at.py"
+
+
+class TestTask10(ParentTest):
+    def test_common_test(self):
+        """ Testing a common stuff. check abstract_test/commontest"""
+
+        self.common_test(filename)
+
+    def test_return_value(self):
+        """ Testing a return value is expected. """
+        sys.path.append(os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..")))
+
+        module = importlib.import_module("11-delete_at")
+        list = [900, 43, 3, 2]
+        index = 2
+        expected = [900, 43, 2]
+
+        result = module.delete_at(list, index)
+
+        self.assertEqual(result, expected)
+
+    def test_return_value2(self):
+        """ Testing a return value is expected. """
+        sys.path.append(os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..")))
+
+        module = importlib.import_module("11-delete_at")
+        list = [900, 43, 3, 2]
+        index = 90
+        expected = [900, 43, 3, 2]
+
+        result = module.delete_at(list, index)
+
+        self.assertEqual(result, expected)
+
+    def test_count_import(self):
+        """ Testing if import is used in the file. """
+
+        target_construct = "import"
+        self.construct_in_use(filename, target_construct)
+
+    def max_not_in_use(self):
+        method = "max"
+        self.method_not_in_use(filename, method)
+
+
+if __name__ == "__main__":
+    unittest.main()
