3367ac0719a629aa44a390a03f11064db4846c67
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-30 18:18:35 +1100

    feat: add 2-square

--- python-classes/README.md
+++ python-classes/README.md
@@ -48,3 +48,14 @@ This project is based on the learning objectives - see the [LEARNING_OBJECTIVES]
 - Instantiation with size (no type/value verification)
 - You are not allowed to import any module
 
+### 2. Size validation
+
+**File:** [2-square.py](https://github.com/Goaty-yagi/holbertonschool-higher_level_programming/blob/main/python-classes/2-square.py)<br>
+**Description:** Write a class Square that defines a square by: (based on 1-square.py).<br>
+**Requirement:** <br>
+- Private instance attribute: size
+- Instantiation with optional size: def __init__(self, size=0):
+-- size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
+-- if size is less than 0, raise a ValueError exception with the message size - must be >= 0
+- You are not allowed to import any module
+
---
+++ python-classes/tests/test2.py
@@ -0,0 +1,48 @@
+import unittest
+import importlib
+import sys
+import os
+
+from parent_test import ParentTest
+
+filename = "2-square.py"
+module = "2-main.py"
+
+
+class TestTask2(ParentTest):
+    def test_common_test(self):
+        """ Testing a common stuff. check abstract_test/commontest"""
+
+        self.common_test(filename)
+
+
+    def test_module_doc(self):
+        """ Testing module doc exists. """
+
+        sys.path.append(os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..")))
+
+        module = importlib.import_module("1-square")
+
+        self.assertTrue(module.__doc__)
+
+    def test_class_doc(self):
+        """ Testing class doc exists. """
+        
+        sys.path.append(os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..")))
+
+        module = importlib.import_module("1-square")
+
+        self.assertTrue(module.Square.__doc__)
+
+    # def test_count_import(self):
+    #     """ Testing if import is used in the file. """
+
+    #     target_construct = "import"
+    #     self.construct_in_use(filename, target_construct)
+    #     # not woring if documentation have "import"
+        
+
+if __name__ == "__main__":
+    unittest.main()
