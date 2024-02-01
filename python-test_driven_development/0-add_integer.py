043d601c04a3d3f4616155d5c25d434c265b6315
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-02-01 16:58:32 +1100

    feat(checker): add 0-add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,26 @@
+"""
+Module: math_operations
+
+This module provides add operation.
+
+Functions:
+    - add_integer(a, b=98): Adds two integers or
+    floats and returns the result as an integer.
+"""
+
+
 def add_integer(a, b=98):
+    """
+    Adds two integers or floats and returns the result as an integer.
+
+    Parameters:
+        a (int or float): The first number to be added.
+        b (int or float, optional): The second number
+        to be added. Defaults to 98.
+
+    Returns:
+        int: The sum of the two numbers, returned as an integer.
+    """
     if not isinstance(a, (int, float)):
         raise ValueError("a must be an integer")
     if not isinstance(b, (int, float)):
