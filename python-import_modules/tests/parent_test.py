74f4d9b85ccd2c4736bd432302b9f56fbda006ba
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-02-03 14:52:22 +1100

    chore: apply test_asset changes for each project

--- python-import_modules/tests/parent_test.py
+++ python-import_modules/tests/parent_test.py
@@ -3,10 +3,9 @@ import sys, os, importlib
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 # Use __import__ for relative import
-module_name = 'test_assets.abstract_test'
+module_name = 'test_assets.custom_test'
 module = importlib.import_module(module_name)
-importlib.import_module(module_name)
 
-class ParentTest(module.AbstractTest):
+class ParentTest(module.CustomTest):
     path = "python-import_modules"
     pass
