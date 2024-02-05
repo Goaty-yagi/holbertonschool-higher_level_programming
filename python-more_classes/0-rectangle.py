be58d2a8021a6fe56634dad724c5c4acec234464
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-02-05 15:31:48 +1100

    docs: add class docs and test associated with it

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+
 """
 This module defines a basic Rectangle class.
 """
 
 
 class Rectangle:
+    """
+    Rectangle Class
+
+    A basic representation of a rectangle.
+    """
     pass
--- python-more_classes/tests/test0.py
+++ python-more_classes/tests/test0.py
@@ -25,12 +25,22 @@ class TestTask0(ParentTest):
 
         self.assertTrue(module.__doc__)
 
+    def test_class_doc(self):
+        """ Testing class doc exists. """
+
+        sys.path.append(os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..")))
+
+        module = importlib.import_module("0-rectangle")
+
+        self.assertTrue(module.Rectangle.__doc__)
+
     def test_count_import(self):
         """ Testing if import is used in the file. """
 
         target_construct = "import"
         self.construct_in_use(filename, target_construct)
-        
+
 
 if __name__ == "__main__":
     unittest.main()
