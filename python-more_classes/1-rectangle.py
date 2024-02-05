9f2c03b6c9b8d6c81c7be35cff5a1d4351d66545
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-02-05 16:19:19 +1100

    docs: modify module, class and methods docs

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -2,6 +2,8 @@
 
 """
 This module defines a basic Rectangle class.
+Classes:
+    - Rectangle: A class representing a rectangle.
 """
 
 
@@ -9,8 +11,27 @@ class Rectangle:
     """
     Rectangle Class
 
-    A basic representation of a rectangle.
+    A basic representation of a rectangle with width and height attributes.
+
+    Attributes:
+        - ATTRIBUTES_NAME (dict): A dictionary mapping attribute names to
+        their string representations.
+
+    Methods:
+        - __init__(self, width: int = 0, height: int = 0) ->
+        None: Constructor to initialize the rectangle's width and height.
+        - attributes_setter(self, value: int, name: str) ->
+        int: Helper method to set and validate attribute values.
+        - width(self) ->
+        int: Getter method for the rectangle's width attribute.
+        - width(self, value: int) ->
+        None: Setter method for the rectangle's width attribute.
+        - height(self) ->
+        int: Getter method for the rectangle's height attribute.
+        - height(self, value: int) ->
+        None: Setter method for the rectangle's height attribute.
     """
+
     ATTRIBUTES_NAME = {
         "WIDTH": "width",
         "HEIGHT": "height"
@@ -36,12 +57,12 @@ class Rectangle:
             raise TypeError("{} must be an integer".format(name))
 
     @property
-    def width(self):
+    def width(self) -> int:
         return self.__width
 
     @width.setter
     def width(self, value: int):
-        return self.attributes_setter(value, "width")
+        self.attributes_setter(value, "width")
 
     @property
     def height(self):
@@ -49,4 +70,4 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        return self.attributes_setter(value, "height")
+        self.attributes_setter(value, "height")
