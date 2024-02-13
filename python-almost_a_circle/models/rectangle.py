db9b0511c7d7d00b85eed1a0628268781f4e4a60
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-02-13 13:11:01 +1100

    docs: add docs for rectangle and square classes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -33,6 +33,19 @@ class Rectangle(Base):
 
     def __init__(self, width: int, height: int, x: int = 0,
                  y: int = 0, id: int = None) -> None:
+        """
+        Initializes an instance representing a rectangle.
+
+        Parameters:
+        - width (int): Width of the rectangle.
+        - height (int): Height of the rectangle.
+        - x (int): X-coordinate of the rectangle (default is 0).
+        - y (int): Y-coordinate of the rectangle (default is 0).
+        - id (int): Identifier for the rectangle (default is None).
+
+        Returns:
+        None
+        """
         self.validator(**{"width": width, "height": height, 'x': x, 'y': y})
         self.__width = width
         self.__height = height
@@ -51,50 +64,139 @@ class Rectangle(Base):
                 raise ValueError("{} must be >= 0".format(key))
 
     def __str__(self) -> str:
+        """
+        Returns a string representation of the rectangle.
+
+        Returns:
+        str: String representation of the rectangle.
+        """
+
         return "[{}] ({}) {}/{} - {}/{}".format(
             __class__.__name__, self.id, self.__x,
             self.__y, self.__width, self.__height)
 
     @property
     def width(self):
+        """
+        Getter method for the width attribute.
+
+        Returns:
+        int: Width of the rectangle.
+        """
+
         return self.__width
 
     @width.setter
     def width(self, value: int) -> None:
+        """
+        Setter method for the width attribute.
+
+        Parameters:
+        - value (int): New value for the width attribute.
+
+        Returns:
+        None
+        """
+
         self.validator(**{"width": value})
         self.__width = value
 
     @property
     def height(self):
+        """
+        Getter method for the height attribute.
+
+        Returns:
+        int: Height of the rectangle.
+        """
+
         return self.__height
 
     @height.setter
     def height(self, value: int) -> None:
+        """
+        Setter method for the height attribute.
+
+        Parameters:
+        - value (int): New value for the height attribute.
+
+        Returns:
+        None
+        """
+
         self.validator(**{"height": value})
         self.__height = value
 
     @property
     def x(self):
+        """
+        Getter method for the x attribute.
+
+        Returns:
+        int: X-coordinate of the rectangle.
+        """
+
         return self.__x
 
     @x.setter
     def x(self, value: int) -> None:
+        """
+        Setter method for the x attribute.
+
+        Parameters:
+        - value (int): New value for the x attribute.
+
+        Returns:
+        None
+        """
+
         self.validator(**{'x': value})
         self.__x = value
 
     @property
     def y(self):
+        """
+        Getter method for the y attribute.
+
+        Returns:
+        int: Y-coordinate of the rectangle.
+        """
+
         return self.__y
 
     @y.setter
     def y(self, value: int) -> None:
+        """
+        Setter method for the y attribute.
+
+        Parameters:
+        - value (int): New value for the y attribute.
+
+        Returns:
+        None
+        """
+
         self.validator(**{'y': value})
         self.__y = value
 
     def area(self) -> int:
+        """
+        Calculates and returns the area of the rectangle.
+
+        Returns:
+        int: Area of the rectangle.
+        """
+
         return self.__height * self.__width
 
     def display(self) -> None:
+        """
+        Prints a representation of the rectangle using '#'.
+
+        Returns:
+        None
+        """
+
         for _ in range(self.__y):
             print()
         x = ' ' * self.__x
@@ -105,6 +207,18 @@ class Rectangle(Base):
             print()
 
     def update(self, *args, **kwargs):
+        """
+        Updates attributes of the rectangle based
+        on positional and keyword arguments.
+
+        Parameters:
+        - *args: Positional arguments representing ID, width, height, x, and y.
+        - **kwargs: Keyword arguments representing attributes to be updated.
+
+        Returns:
+        None
+        """
+
         ATTR_NAMES = {
             "ID": "id",
             "WIDTH": "width",
@@ -139,6 +253,13 @@ class Rectangle(Base):
                     self.__height = val
 
     def to_dictionary(self):
+        """
+        Returns a dictionary representation of the rectangle.
+
+        Returns:
+        dict: Dictionary representation of the rectangle.
+        """
+
         temp_dict = {}
         for i in Rectangle.ATTR_NAMES.values():
             temp_dict[i] = getattr(self, i)
--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -24,25 +24,68 @@ class Square(Rectangle):
     }
 
     def __init__(self, size: int, x: int = 0, y: int = 0, id: int = None):
+        """
+        Initializes an instance of the Square class.
+
+        Parameters:
+        - size (int): The side length of the square.
+        - x (int): X-coordinate of the square's position (default is 0).
+        - y (int): Y-coordinate of the square's position (default is 0).
+        - id (int): An optional identifier for the square (default is None).
+
+        Returns:
+        None
+        """
         super().__init__(size, size, x, y, id)
         self.__size = size
 
     def __str__(self) -> str:
+        """
+        Returns a string representation of the square.
+
+        Returns:
+        str: String representation of the square.
+        """
         return "[{}] ({}) {}/{} - {}".format(
             __class__.__name__, self.id, self.x,
             self.y, self.height)
 
     @property
     def size(self):
+        """
+        Getter method for the size attribute.
+
+        Returns:
+        int: The side length of the square.
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Setter method for the size attribute.
+
+        Parameters:
+        - value: New value for the size attribute.
+
+        Returns:
+        None
+        """
         self.__size = value
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """
+        Updates attributes of the square based on positional and keyword arguments.
+
+        Parameters:
+        - *args: Positional arguments representing ID, size, x, and y.
+        - **kwargs: Keyword arguments representing attributes to be updated.
+
+        Returns:
+        None
+        """
         ATTR_NAMES = {
             "ID": "id",
             "SIZE": "size",
@@ -71,6 +114,12 @@ class Square(Rectangle):
                     self.size = val
 
     def to_dictionary(self):
+        """
+        Returns a dictionary representation of the square.
+
+        Returns:
+        dict: Dictionary representation of the square.
+        """
         temp_dict = {}
         for i in Square.ATTR_NAMES.values():
             temp_dict[i] = getattr(self, i)
