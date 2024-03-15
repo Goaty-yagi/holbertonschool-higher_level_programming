d2def5228ba34e5e9c5e537c11535caec2cde8fd
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-03-15 15:21:39 +1100

    fix: uppercase as

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -6,6 +6,6 @@ LEFT JOIN (
     FROM tv_show_genres
     JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
     WHERE tv_genres.name = "Comedy"
-) as linked_genres ON tv_shows.id = linked_genres.show_id
+) AS linked_genres ON tv_shows.id = linked_genres.show_id
 WHERE linked_genres.show_id IS NULL
 ORDER BY tv_shows.title;
