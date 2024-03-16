e3edcac51359c80dd2130214b603f023bfb975d2
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-03-16 21:57:35 +1100

    chore: modify creating process

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -19,10 +19,6 @@ if __name__ == "__main__":
     )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session = Session()
-    new_state = State(name="California")
-    new_city = City(name="San Francisco", state=new_state)
-    session.add(new_state)
-    session.add(new_city)
-    session.commit()
-    session.close()
+    with Session() as session:
+        session.add(City(name="San Francisco", state=State(name="California")))
+        session.commit()
