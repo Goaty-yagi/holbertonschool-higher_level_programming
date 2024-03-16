02d825e64cfdf5f91271bb469c0413898bbd2caf
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-03-16 22:13:59 +1100

    fix: import relationship class

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -6,8 +6,8 @@ import sys
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-from model_state import Base, State
-from model_city import City
+from relationship_state import State
+from relationship_city import Base, City
 
 if __name__ == "__main__":
     username = sys.argv[1]
