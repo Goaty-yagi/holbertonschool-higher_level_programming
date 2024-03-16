78e1dbeee90d3a31523f0c863a3fdb2129003c9a
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-03-16 14:07:57 +1100

    docs: add docs to model_state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This module provides State class
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -6,8 +9,10 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    This class inherited from Base class to represent a table named state
+    """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
